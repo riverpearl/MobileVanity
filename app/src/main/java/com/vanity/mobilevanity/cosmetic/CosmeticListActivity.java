032cef2bb1ce2c6672c332a20f9f6dafd0a9da43
River Pearl <riverpearl0115@gmail.com>
2016-08-23 15:06:42 +0900

    mypage, cosmetic package

---
+++ app/src/main/java/com/vanity/mobilevanity/cosmetic/CosmeticListActivity.java
@@ -0,0 +1,15 @@
+package com.vanity.mobilevanity.cosmetic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vanity.mobilevanity.R;
+
+public class CosmeticListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cosmetic_list);
+    }
+}
