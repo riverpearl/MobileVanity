34fd326675d721b1a4684c122a373b7e564632c8
Eunmi Hwang <cncn6666@naver.com>
2016-08-23 17:21:31 +0900

    renewal HomeFragment

--- app/src/main/java/com/vanity/mobilevanity/cosmetic/HomeFragment.java
+++ app/src/main/java/com/vanity/mobilevanity/cosmetic/HomeFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 import com.vanity.mobilevanity.R;
 import com.vanity.mobilevanity.alert.AlertActivity;
+import com.vanity.mobilevanity.beautytip.BeautyTipDetailActivity;
 import com.vanity.mobilevanity.beautytip.BeautyTipFragment;
 import com.vanity.mobilevanity.mypage.MyPageFragment;
 import com.vanity.mobilevanity.register.RegisterBarcodeActivity;
@@ -39,41 +40,9 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    @OnClick(R.id.btn_alarm)
-    public void onAlarmClick(View view) {
-        Intent intent = new Intent(getContext(), AlertActivity.class);
+    @OnClick(R.id.btn_makeup)
+    public void onMakeupClick(View view) {
+        Intent intent = new Intent(getContext(), CosmeticListActivity.class);
         startActivity(intent);
     }
-
-    @OnClick(R.id.btn_floating)
-    public void onFloatingClick(View view) {
-        Intent intent = new Intent(getContext(), RegisterBarcodeActivity.class);
-        startActivity(intent);
-    }
-
-    @OnClick(R.id.btn_home)
-    public void onHomeClick(View view) {
-        Intent intent = new Intent(getContext(), HomeFragment.class);
-        startActivity(intent);
-    }
-
-    @OnClick(R.id.btn_beautytip)
-    public void onBeautyTipClick(View view) {
-        Intent intent = new Intent(getContext(), BeautyTipFragment.class);
-        startActivity(intent);
-    }
-
-    @OnClick(R.id.btn_sale)
-    public void onSaleClick(View view) {
-        Intent intent = new Intent(getContext(), SaleFragment.class);
-        startActivity(intent);
-    }
-
-    @OnClick(R.id.btn_mypage)
-    public void onMypageClick(View view) {
-        Intent intent = new Intent(getContext(), MyPageFragment.class);
-        startActivity(intent);
-    }
-
-
 }
