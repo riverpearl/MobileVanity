107dff20490a5eff9583f053330e134812148a11
Eunmi Hwang <cncn6666@naver.com>
2016-08-23 11:56:11 +0900

    main beautytip package

---
+++ app/src/main/java/com/vanity/mobilevanity/beautytip/BeautyTipWriteActivity.java
@@ -0,0 +1,15 @@
+package com.vanity.mobilevanity.beautytip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vanity.mobilevanity.R;
+
+public class BeautyTipWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beauty_tip_write);
+    }
+}
