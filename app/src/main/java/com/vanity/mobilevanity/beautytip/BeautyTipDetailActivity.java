426980c65d5c572b40381ec79adb2e775d2ff187
Eunmi Hwang <cncn6666@naver.com>
2016-08-24 18:12:05 +0900

    BeautyTipDetailActivity T-102

--- app/src/main/java/com/vanity/mobilevanity/beautytip/BeautyTipDetailActivity.java
+++ app/src/main/java/com/vanity/mobilevanity/beautytip/BeautyTipDetailActivity.java
@@ -2,7 +2,10 @@ package com.vanity.mobilevanity.beautytip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.vanity.mobilevanity.R;
 
@@ -23,4 +26,19 @@ public class BeautyTipDetailActivity extends AppCompatActivity {
         this.finish();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_beauty_tip_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_update:
+            case R.id.menu_delete:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
