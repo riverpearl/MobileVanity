e5ca87ecf4a8871b5374c6ff8a910b54d707f43f
Eunmi Hwang <cncn6666@naver.com>
2016-08-24 17:16:37 +0900

    BeautyTipWriteActivity T-201

--- app/src/main/java/com/vanity/mobilevanity/beautytip/BeautyTipWriteActivity.java
+++ app/src/main/java/com/vanity/mobilevanity/beautytip/BeautyTipWriteActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.vanity.mobilevanity.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class BeautyTipWriteActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_title)
+    EditText titleView;
+
+    @BindView(R.id.text_content)
+    EditText contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +34,6 @@ public class BeautyTipWriteActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_set)
     public void onSetClick(View view) {
-        finish();
+       finish();
     }
 }
