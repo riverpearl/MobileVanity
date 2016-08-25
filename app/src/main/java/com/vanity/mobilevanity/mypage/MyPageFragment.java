a870e51e5825f06031a4b8a98031a50a3106b9a2
River Pearl <riverpearl0115@gmail.com>
2016-08-25 11:55:37 +0900

    update profile activity

--- app/src/main/java/com/vanity/mobilevanity/mypage/MyPageFragment.java
+++ app/src/main/java/com/vanity/mobilevanity/mypage/MyPageFragment.java
@@ -35,6 +35,9 @@ public class MyPageFragment extends Fragment {
     @BindView(R.id.text_nickname)
     TextView nicknameView;
 
+    @BindView(R.id.text_gender)
+    TextView genderView;
+
     @BindView(R.id.text_skin_type)
     TextView skinTypeView;
 
@@ -47,10 +50,26 @@ public class MyPageFragment extends Fragment {
     @BindView(R.id.text_lip_count)
     TextView lipCountView;
 
+    @BindView(R.id.text_skin_count)
+    TextView skinCountView;
+
     @BindView(R.id.text_base_count)
     TextView baseCountView;
 
+    @BindView(R.id.text_cleansing_count)
+    TextView cleansingCountView;
+
+    @BindView(R.id.text_tool_count)
+    TextView toolCountView;
+
+    User user;
+
+    public final static String TAG_NICKNAME = "nickname";
+    public final static String TAG_SKIN_TYPE = "skintype";
+    public final static String TAG_SKIN_TONE = "skintone";
+    public final static String TAG_GENDER = "gender";
     public final static String TAG_ITEM_TAB = "item";
+
     public final static int INDEX_ITEM_EYE = 1;
     public final static int INDEX_ITEM_LIP = 2;
     public final static int INDEX_ITEM_SKIN = 3;
@@ -63,8 +82,68 @@ public class MyPageFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_my_page, container, false);
         ButterKnife.bind(this, view);
+        init();
 
         return view;
+    }
+
+    private void init() {
+        user = new User();
+        user.setUserNickname("Vanity");
+        user.setGender(2);
+        user.setSkinType(2);
+        user.setSkinTone(2);
+        user.setEyeNum(10);
+        user.setLipNum(10);
+        user.setSkinNum(10);
+        user.setBaseNum(10);
+        user.setCleansingNum(10);
+        user.setToolNum(10);
+
+        nicknameView.setText(user.getUserNickname());
+
+        switch (user.getGender()) {
+            case 1 : default :
+                genderView.setText("남성");
+                break;
+            case 2 :
+                genderView.setText("여성");
+                break;
+        }
+
+        switch (user.getSkinType()) {
+            case 1 : default :
+                skinTypeView.setText("건성");
+                break;
+            case 2 :
+                skinTypeView.setText("중성");
+                break;
+            case 3 :
+                skinTypeView.setText("지성");
+                break;
+            case 4 :
+                skinTypeView.setText("복합성");
+                break;
+        }
+
+        switch (user.getSkinTone()) {
+            case 1 : default :
+                skinToneView.setText("13호");
+                break;
+            case 2 :
+                skinToneView.setText("21호");
+                break;
+            case 3 :
+                skinToneView.setText("23호");
+                break;
+        }
+
+        eyeCountView.setText(user.getEyeNum() + "");
+        lipCountView.setText(user.getLipNum() + "");
+        skinCountView.setText(user.getSkinNum() + "");
+        baseCountView.setText(user.getBaseNum() + "");
+        cleansingCountView.setText(user.getCleansingNum() + "");
+        toolCountView.setText(user.getToolNum() + "");
     }
 
     @OnClick(R.id.btn_logout)
@@ -77,6 +156,10 @@ public class MyPageFragment extends Fragment {
     @OnClick(R.id.btn_modification)
     public void onModificationClick(View view) {
         Intent intent = new Intent(getContext(), UpdateProfileActivity.class);
+        intent.putExtra(TAG_NICKNAME, user.getUserNickname());
+        intent.putExtra(TAG_GENDER, user.getGender());
+        intent.putExtra(TAG_SKIN_TYPE, user.getSkinType());
+        intent.putExtra(TAG_SKIN_TONE, user.getSkinTone());
         startActivity(intent);
     }
 
--- app/src/main/java/com/vanity/mobilevanity/mypage/UpdateProfileActivity.java
+++ app/src/main/java/com/vanity/mobilevanity/mypage/UpdateProfileActivity.java
@@ -6,20 +6,82 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.vanity.mobilevanity.R;
 import com.vanity.mobilevanity.alert.AlertActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class UpdateProfileActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_nickname)
+    TextView nicknameView;
+
+    @BindView(R.id.group_select_gender)
+    RadioGroup groupGenderView;
+
+    @BindView(R.id.group_select_skin_type)
+    RadioGroup groupSkinTypeView;
+
+    @BindView(R.id.group_select_skin_tone)
+    RadioGroup groupSkinToneView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
         ButterKnife.bind(this);
+
+        init();
+    }
+
+    private void init() {
+        Intent intent = getIntent();
+        int gender = intent.getIntExtra(MyPageFragment.TAG_GENDER, 1);
+        int type = intent.getIntExtra(MyPageFragment.TAG_SKIN_TYPE, 1);
+        int tone = intent.getIntExtra(MyPageFragment.TAG_SKIN_TONE, 1);
+
+        nicknameView.setText(intent.getStringExtra(MyPageFragment.TAG_NICKNAME));
+
+        switch (gender) {
+            case 1 :
+                groupGenderView.check(R.id.radio_male);
+                break;
+            case 2 :
+                groupGenderView.check(R.id.radio_female);
+                break;
+        }
+
+        switch (type) {
+            case 1 :
+                groupSkinTypeView.check(R.id.radio_dry);
+                break;
+            case 2 :
+                groupSkinTypeView.check(R.id.radio_normal);
+                break;
+            case 3 :
+                groupSkinTypeView.check(R.id.radio_oily);
+                break;
+            case 4 :
+                groupSkinTypeView.check(R.id.radio_complex);
+                break;
+        }
+
+        switch (tone) {
+            case 1 :
+                groupSkinToneView.check(R.id.radio_13);
+                break;
+            case 2 :
+                groupSkinToneView.check(R.id.radio_21);
+                break;
+            case 3 :
+                groupSkinToneView.check(R.id.radio_23);
+                break;
+        }
     }
 
     @OnClick(R.id.btn_set)
