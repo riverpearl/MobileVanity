4b90e13b196ea16e16ec98a0844a6d0706f0f8ef
Eunmi Hwang <cncn6666@naver.com>
2016-08-29 17:42:24 +0900

    BeautyTipWriteActivity T-201

--- app/src/main/java/com/vanity/mobilevanity/beautytip/BeautyTipWriteActivity.java
+++ app/src/main/java/com/vanity/mobilevanity/beautytip/BeautyTipWriteActivity.java
@@ -1,14 +1,23 @@
 package com.vanity.mobilevanity.beautytip;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.vanity.mobilevanity.R;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,17 +31,55 @@ public class BeautyTipWriteActivity extends AppCompatActivity {
     @BindView(R.id.text_content)
     EditText contentView;
 
+    @BindView(R.id.image_content)
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beauty_tip_write);
         ButterKnife.bind(this);
+
+        Button camera = (Button) findViewById(R.id.btn_camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent, RC_GET_IMAGE);
+            }
+        });
     }
+
+    private static final int RC_GET_IMAGE = 1;
 
     @OnClick(R.id.btn_set)
     public void onSetClick(View view) {
         finish();
     }
+
+    File uploadFile = null;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_GET_IMAGE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = data.getData();
+                Cursor c = getContentResolver().query(uri, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
+
+                if (c.moveToNext()) {
+                    String path = c.getString(c.getColumnIndex(MediaStore.Images.Media.DATA));
+                    uploadFile = new File(path);
+
+                    Glide.with(this)
+                            .load(uploadFile)
+                            .into(imageView);
+                }
+            }
+        }
+    }
+    // 갤러리
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
