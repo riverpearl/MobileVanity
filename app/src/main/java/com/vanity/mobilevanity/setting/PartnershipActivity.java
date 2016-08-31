9330dd5c6b8dd0f67a453f7f670b7537d9e35394
River Pearl <riverpearl0115@gmail.com>
2016-08-31 19:55:48 +0900

    partnership activity

--- app/src/main/java/com/vanity/mobilevanity/setting/PartnershipActivity.java
+++ app/src/main/java/com/vanity/mobilevanity/setting/PartnershipActivity.java
@@ -4,18 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.vanity.mobilevanity.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class PartnershipActivity extends AppCompatActivity {
+
+    @BindView(R.id.text_partnership)
+    TextView partnershipView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partnership);
+
+        ButterKnife.bind(this);
+
+        partnershipView.setText("함께 만들어가는 베니띠.\n" +
+            "제휴를 원하시는 경우 아래 주소로 연락주세요.\n\n\n\n\n" +
+            "help@mobilevanity.com");
     }
 
-    @Override
+   @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.actionbar_cancel, menu);
         return true;
