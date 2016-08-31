689d785449f565551f4cb871ebd772cdd7f3a006
River Pearl <riverpearl0115@gmail.com>
2016-08-31 19:48:22 +0900

    faq activity

--- app/src/main/java/com/vanity/mobilevanity/setting/FAQActivity.java
+++ app/src/main/java/com/vanity/mobilevanity/setting/FAQActivity.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 import com.vanity.mobilevanity.R;
 import com.vanity.mobilevanity.adapter.FAQAdapter;
 import com.vanity.mobilevanity.data.FAQ;
+import com.vanity.mobilevanity.data.NetworkResult;
+import com.vanity.mobilevanity.manager.NetworkManager;
+import com.vanity.mobilevanity.manager.NetworkRequest;
+import com.vanity.mobilevanity.request.FAQListRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,28 +36,38 @@ public class FAQActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        init();
-        listView.setAdapter(faqAdapter);
+        FAQListRequest request = new FAQListRequest(this);
+        NetworkManager.getInstance().getNetworkData(request, new NetworkManager.OnResultListener<NetworkResult<List<FAQ>>>() {
+            @Override
+            public void onSuccess(NetworkRequest<NetworkResult<List<FAQ>>> request, NetworkResult<List<FAQ>> result) {
+                if (result.getCode() == 1) {
+                    List<FAQ> faqs = result.getResult();
+                    List<FAQ> list = new ArrayList<>();
+
+                    for (int i = 0; i < faqs.size(); i++) {
+                        List<String> content = new ArrayList<>();
+                        content.add(faqs.get(i).getContent());
+
+                        FAQ faq = new FAQ();
+                        faq.setTitle(faqs.get(i).getTitle());
+                        faq.setChild(content);
+
+                        list.add(faq);
+                    }
+
+                    faqAdapter = new FAQAdapter(FAQActivity.this, list);
+                    listView.setAdapter(faqAdapter);
+                }
+            }
+
+            @Override
+            public void onFail(NetworkRequest<NetworkResult<List<FAQ>>> request, int errorCode, String errorMessage, Throwable e) {
+
+            }
+        });
 
         LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         listView.setLayoutManager(manager);
-    }
-
-    private void init() {
-        List<FAQ> list = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            List<String> content = new ArrayList<>();
-            content.add("content " + i);
-
-            FAQ faq = new FAQ();
-            faq.setTitle("title " + i);
-            faq.setChild(content);
-
-            list.add(faq);
-        }
-
-        faqAdapter = new FAQAdapter(this, list);
     }
 
     @Override
